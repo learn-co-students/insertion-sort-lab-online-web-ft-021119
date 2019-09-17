dcb9779afacb8058e1370d8d89e86de0f7f102fe
bebopvinh <vinhdo07@gmail.com>
2019-09-16 17:05:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+   let minNum = array[0]
+   let minIndex = 0
+   array.forEach((num, index) => {
+      if (num <= minNum) {
+         minNum = num
+         minIndex = index
+      }
+   })
+   array = array.splice(minIndex, 1)
+   return minNum
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+   let sorted = []
+   let minNum
+   while (array.length > 0) {
+      minNum = findMinAndRemove(array)
+      sorted.push(minNum)
+   }
+   return sorted
 }
